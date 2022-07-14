36882a3abf76167ea101ea9c8a5156eef86b2f67
Obada Khalili <obada.akhalili@gmail.com>
2022-07-14 06:54:27 +0300

    add unit tests for when key not found in data object

--- test/sprightly.test.ts
+++ test/sprightly.test.ts
@@ -13,5 +13,29 @@ jest.mock("fs/promises", () => {
 
 test("good first test", async () => {
   const doc = await sprightly("my-name", { myName: "Obada", hisName: "Osid" })
-  expect(doc).toMatchSnapshot()
+  expect(doc).toMatchSnapshot(`"sprightly"`)
+})
+
+test("if key doesn't exist, then the resolved value should be `keyFallback`'s default value whcih is empty string", async () => {
+  const doc = await sprightly("my-name", { hisName: "Osid" })
+  expect(doc).toMatchSnapshot(`"sprightly"`)
+})
+
+test("if key doesn't exist and the `keyFallback` is set, then the resolved value should be the value set to `keyFallback`", async () => {
+  const doc = await sprightly(
+    "my-name",
+    { hisName: "Osid" },
+    { keyFallback: "Obada" },
+  )
+  expect(doc).toMatchSnapshot(`"sprightly"`)
+})
+
+test("if key doesn't exist and the `throwOnKeyNotfound` is set to true, then an error should be thrown", async () => {
+  await expect(
+    sprightly(
+      "key-not-found",
+      { hisName: "Osid" },
+      { throwOnKeyNotfound: true },
+    ),
+  ).rejects.toMatchSnapshot()
 })
