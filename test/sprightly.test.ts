1ab1680a2789358aad988ea196eee8bf270ed7fc
Obada Khalili <obada.akhalili@gmail.com>
2022-07-14 07:13:50 +0300

    add unit tests for nested properties and array indexing, and use inline snapshots

--- test/sprightly.test.ts
+++ test/sprightly.test.ts
@@ -2,7 +2,7 @@ import sprightly from "../src/sprightly"
 
 jest.mock("fs/promises", () => {
   const dir: Record<string, string> = {
-    "my-name": `my name is {{ myName }}. << ../his-name >>`,
+    "my-name": `my name is {{ myName }}.\n{{> ../his-name }}`,
     "../his-name": `his name is {{ hisName }}`,
     nested: "my name is {{ name.first }} {{ name.last }}",
     "array-indexing": "my name is {{ name[0] }} {{ name[1] }}",
@@ -15,12 +15,12 @@ jest.mock("fs/promises", () => {
 
 test("good first test", async () => {
   const doc = await sprightly("my-name", { myName: "Obada", hisName: "Osid" })
-  expect(doc).toMatchSnapshot(`"sprightly"`)
+  expect(doc).toMatchInlineSnapshot(`"sprightly"`)
 })
 
 test("if key doesn't exist, then the resolved value should be `keyFallback`'s default value whcih is empty string", async () => {
   const doc = await sprightly("my-name", { hisName: "Osid" })
-  expect(doc).toMatchSnapshot(`"sprightly"`)
+  expect(doc).toMatchInlineSnapshot(`"sprightly"`)
 })
 
 test("if key doesn't exist and the `keyFallback` is set, then the resolved value should be the value set to `keyFallback`", async () => {
@@ -29,7 +29,7 @@ test("if key doesn't exist and the `keyFallback` is set, then the resolved value
     { hisName: "Osid" },
     { keyFallback: "Obada" },
   )
-  expect(doc).toMatchSnapshot(`"sprightly"`)
+  expect(doc).toMatchInlineSnapshot(`"sprightly"`)
 })
 
 test("if key doesn't exist and the `throwOnKeyNotfound` is set to true, then an error should be thrown", async () => {
@@ -39,19 +39,19 @@ test("if key doesn't exist and the `throwOnKeyNotfound` is set to true, then an 
       { hisName: "Osid" },
       { throwOnKeyNotfound: true },
     ),
-  ).rejects.toMatchSnapshot()
+  ).rejects.toMatchInlineSnapshot(`[Error: sprightly error]`)
 })
 
 test("nested properties are resolved correctly", async () => {
   const doc = await sprightly("nested", {
     name: { first: "Obada", last: "Khalili" },
   })
-  expect(doc).toMatchSnapshot()
+  expect(doc).toMatchInlineSnapshot(`"sprightly"`)
 })
 
 test("array indexing is resolved correctly", async () => {
   const doc = await sprightly("array-indexing", {
     name: ["Obada", "Khalili"],
   })
-  expect(doc).toMatchSnapshot()
+  expect(doc).toMatchInlineSnapshot(`"sprightly"`)
 })
